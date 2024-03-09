5b26eae5e7a5e3a2efc80ab5cee82f4d1b629056
Christian Avemark <christian@avemark.se>
2024-03-09 20:29:19 +0100

    clean up in aisle lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,6 @@
 #![allow(dead_code)]
-use std::net::UdpSocket;
-use std::sync::atomic::Ordering::{AcqRel, Relaxed};
-use std::sync::atomic::{AtomicBool, AtomicUsize};
-use std::sync::{Arc, Condvar, Mutex};
-pub mod listener;
 
-mod logdata;
-mod logline;
-
-pub fn listen<const N: usize, const M: usize>(
-    data: Arc<[Mutex<[u8; M]>; N]>,
-    interrupted: &AtomicBool,
-    socket: UdpSocket,
-    reference: Arc<Reference<N>>,
-) {
-    while !interrupted.load(Relaxed) {
-        socket
-            .recv_from(&mut *data[reference.increment()].lock().unwrap())
-            .expect("Could not listen on UDP socket");
-        reference.notify();
-    }
-}
-
-pub struct Reference<const N: usize> {
-    cond: Condvar,
-    reference: AtomicUsize,
-}
-
-impl<const N: usize> Reference<N> {
-    fn increment(&self) -> usize {
-        self.reference.fetch_add(1, AcqRel);
-        self.reference
-            .compare_exchange(N, 0usize, AcqRel, Relaxed)
-            .unwrap_or_else(|num| num)
-    }
-
-    fn notify(&self) {
-        self.cond.notify_all();
-    }
-}
+pub mod logdata;
+pub mod logline;
+pub mod receiver;
+pub mod udp;
